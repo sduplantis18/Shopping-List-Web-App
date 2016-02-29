0ba5c0a6beb1363e8eecae77c8dda28cced1a5f9
sduplantis18 <sduplantis18@gmail.com>
2016-02-28 22:25:07 -0500

    updated js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 
-$(document).ready(function () {
-    $('#add').click(function)() {
-  var input = $('#items');
-  $(.grocery - list)
-    .append('<li class="item">' + input.val + '</li>');
+$(function() {
+$('#add').click(function () {
+  var input = $('#enter-items');
+  $('.grocery-list')
+    .append('<li class="item">' + input.val() + '</li>');
   input.val('');
-    });	
+});
 });
